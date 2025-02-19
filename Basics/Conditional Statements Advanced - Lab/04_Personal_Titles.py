066599e4556498ad32886e50de88257fef827e2e
metalonov <118399788+metalonov@users.noreply.github.com>
2025-02-19 19:58:01 +0200

    afs

--- Basics/Conditional Statements Advanced - Lab/04_Personal_Titles.py
+++ Basics/Conditional Statements Advanced - Lab/04_Personal_Titles.py
@@ -1,1 +1,15 @@
 # 4. Personal Titles
+
+enterAge = float(input())
+enterGender = input()
+
+if enterGender == "m" and enterAge >= 16 : 
+	print("Mr.")
+elif enterGender == "m" and enterAge < 16 : 
+	print("Master")
+elif enterGender == "f" and enterAge >= 16 :
+	print("Ms.")
+elif enterGender == "f" and enterAge < 16 :
+	print("Miss")
+else :
+	print("incorrect data")
